fc692798e10fa9f8379ac7a71f03b2735db11aea
Jared Loosli <jloosli@gmail.com>
2017-01-12 12:12:35 -0700

    Cleaned up JS file

--- public/js/test.js
+++ public/js/test.js
@@ -1,10 +1,10 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBNJNY1EiImCdjhQqb2bZEHzZo2ralIqdQ",
-  authDomain: "oliver-dexterity.firebaseapp.com",
-  databaseURL: "https://oliver-dexterity.firebaseio.com",
-  storageBucket: "oliver-dexterity.appspot.com",
-  messagingSenderId: "576315655284"
+    apiKey: "AIzaSyBNJNY1EiImCdjhQqb2bZEHzZo2ralIqdQ",
+    authDomain: "oliver-dexterity.firebaseapp.com",
+    databaseURL: "https://oliver-dexterity.firebaseio.com",
+    storageBucket: "oliver-dexterity.appspot.com",
+    messagingSenderId: "576315655284"
 };
 firebase.initializeApp(config);
 
@@ -16,96 +16,101 @@ var text = document.getElementById('text');
 var countdown_start = 5;
 var max_test = 5;
 var min_test = 1;
-var test_length = 5;
 var testing = false;
 var timer;
 var result;
 var results = [];
 
 function run_tests() {
-  console.log(results);
-  if (results.length == tests_to_run) {
-    finalize();
-    return;
-  }
-  if (results.length < (tests_to_run / 2)) {
-    test('left');
-  } else {
-    test('right');
-  }
+    console.log(results);
+    if (results.length === tests_to_run) {
+        finalize();
+        return;
+    }
+    if (results.length < (tests_to_run / 2)) {
+        test('left');
+    } else {
+        test('right');
+    }
 
 }
 function countdown(start) {
-  if (start > 0) {
-    text.innerHTML = start;
-    setTimeout(function () {
-      countdown(start - 1);
-    }, 1000);
-  } else {
-    text.innerHTML = '';
-    run_tests();
-  }
+    if (start > 0) {
+        text.innerHTML = start;
+        setTimeout(function () {
+            countdown(start - 1);
+        }, 1000);
+    } else {
+        text.innerHTML = '';
+        run_tests();
+    }
 }
 
 function test(hand) {
-  updateProgress();
-  title.innerHTML = "Use your <span class='text-danger'>" + hand + "</span> hand to tap the screen or keyboard when it flashes GO!";
-  document.body.className = hand;
-  if(hand == 'right') {
-    document.querySelector('.d-flex').classList.add('flex-row-reverse');
-  }
-  setTimeout(function () {
-    text.innerHTML = "GO!";
-    document.body.style = "background: slateblue; color: white;"
-    testing = true;
-    timer = Date.now();
-  }, ((Math.random() * (max_test - min_test)) + min_test) * 1000);
+    updateProgress();
+    title.innerHTML = "Use your <span class='text-danger'>" + hand + "</span> hand to tap the screen or keyboard when it flashes GO!";
+    document.body.className = hand;
+    if (hand === 'right') {
+        document.querySelector('.d-flex').classList.add('flex-row-reverse');
+    }
+    setTimeout(function () {
+        text.innerHTML = "GO!";
+        document.body.style = "background: slateblue; color: white;";
+        testing = true;
+        timer = Date.now();
+    }, ((Math.random() * (max_test - min_test)) + min_test) * 1000);
 }
 
 function catchEvent(event) {
-  if (testing) {
-    console.log(event);
-    testing = false;
-    document.body.style = "background: white; color: black;"
-    result = (Date.now() - timer) / 1000;
-    console.log(result, 'seconds');
-    text.innerHTML = "";
-    results.push(result);
-    run_tests();
-  }
+    if (testing) {
+        console.log(event);
+        testing = false;
+        document.body.style = "background: white; color: black;";
+        result = (Date.now() - timer) / 1000;
+        console.log(result, 'seconds');
+        text.innerHTML = "";
+        results.push(result);
+        run_tests();
+    }
 }
 
 var getQueryParameters = function (str) {
-  return (str || document.location.search)
-    .replace(/(^\?)/, '')
-    .split("&")
-    .map(function (n) { return n = n.split("="), this[n[0]] = n[1], this }
-      .bind({})
-    )[0];
-}
+    return (str || document.location.search)
+        .replace(/(^\?)/, '')
+        .split("&")
+        .map(function (n) {
+                return n = n.split("="), this[n[0]] = n[1], this
+            }
+                .bind({})
+        )[0];
+};
 
 function updateProgress() {
-  var pct = Math.round(results.length / (max_test + 1) * 100);
-  var bar = document.querySelector('.progress-bar');
-  bar.innerHTML = pct + '%';
-  bar.style = "width: " + bar.innerHTML;
-  bar.setAttribute('aria-valuenow', pct);
+    var pct = Math.round(results.length / (max_test + 1) * 100);
+    var bar = document.querySelector('.progress-bar');
+    bar.innerHTML = pct + '%';
+    bar.style = "width: " + bar.innerHTML;
+    bar.setAttribute('aria-valuenow', pct);
 }
 
 function finalize() {
-  // Create a new post reference with an auto-generated id
-  var leftAvg = results.slice(0, results.length / 2).reduce(function (sum, n) { return sum + n }, 0) / (results.length / 2);
-  var rightAvg = results.slice(results.length / 2).reduce(function (sum, n) { return sum + n }, 0) / (results.length / 2);
-  var dataToSave = {
-    date: Date.now(),
-    tester: getQueryParameters(),
-    data: results,
-    avg: { left: leftAvg, right: rightAvg }
-  };
-  var newTestResultRef = database.ref('tests').push();
-  newTestResultRef.set(dataToSave).then(function () {
-    window.location = '/page4.html';
-  });
+    // Create a new post reference with an auto-generated id
+    var leftAvg = results.slice(0, results.length / 2).reduce(function (sum, n) {
+            return sum + n
+        }, 0) / (results.length / 2);
+    var rightAvg = results.slice(results.length / 2).reduce(function (sum, n) {
+            return sum + n
+        }, 0) / (results.length / 2);
+    var dataToSave = {
+        date: Date.now(),
+        tester: getQueryParameters(),
+        data: results,
+        avg: {left: leftAvg, right: rightAvg}
+    };
+    var newTestResultRef = database.ref('tests').push();
+    newTestResultRef.set(dataToSave).then(function () {
+        window.location = '/page4.html';
+    });
 }
 
 window.addEventListener('keypress', catchEvent, false);
